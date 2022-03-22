03024a0a4f6e465d548ee7697755379a7797b293
SkwalExe <support@skwal.net>
2022-03-22 06:08:09 +0100

    💠 add comments

--- index.js
+++ index.js
@@ -13,44 +13,46 @@
 
 const pupeteer = require('puppeteer');
 
-async function sleep(ms) {
+async function sleep(ms) { // function to sleep in milliseconds
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
 
 
-(async() => {
+(async () => {
     const browser = await pupeteer.launch({
         headless: false,
         defaultViewport: null
     });
+
     const page = await browser.newPage();
-    await page.goto('https://play.typeracer.com/');
+    await page.goto('https://play.typeracer.com/'); // open typeracer
 
 
 
 
     await page.waitForSelector('#gwt-uid-1 > a');
-    await page.click('#gwt-uid-1 > a');
+    await page.click('#gwt-uid-1 > a'); // click to button to start a race
 
 
-    await sleep(14000);
+    await sleep(14000); // wait 14 seconds - time for the game to start
+
     page.evaluate(() => {
+        // focus the input
         document.querySelector("#gwt-uid-20 > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td > input").focus()
     });
-    await sleep(1000)
+
+    await sleep(1000) // wait 1 second
 
     var text = await page.$('#gwt-uid-20 > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div');
+    var text = await page.evaluate(text => text.textContent, text); // get the text to type
 
 
-    var text = await page.evaluate(text => text.textContent, text);
 
-    for (var i = 0; i < text.length; i++) {
-        await sleep(35);
-        page.keyboard.type(text[i]);
-
+    for (var i = 0; i < text.length; i++) { // for each chars in the text
+        await sleep(35); // delay between each char
+        page.keyboard.type(text[i]); // type the char
     }
-
 
 })();
